fa0109fac44ec78703173186e2bd754d749b8381
hyeyeah <dgm119@hanmail.net>
2020-04-21 17:51:42 +0900

    auth users routes

---
+++ models/user.js
@@ -0,0 +1,53 @@
+const mongoose = require("mongoose");
+const Joi = require("joi");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    minlength: 5,
+    maxlength: 255
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6,
+    maxlength: 1024
+  },
+  //   name: {
+  //        type: String,
+  //        minlength: 1,
+  //        maxlength: 50
+  //     },
+  isAdmin: Boolean
+});
+
+userSchema.methods.generateAuthToken = function() {
+  const token = jwt.sign(
+    { _id: this._id, isAdmin: this.isAdmin },
+    config.get("jwtPrivateKey")
+  );
+  return token;
+};
+
+const User = mongoose.model("User", userSchema);
+
+function validateUser(user) {
+  const schema = {
+    email: Joi.string()
+      .min(5)
+      .max(255)
+      .required()
+      .email(),
+    password: Joi.string()
+      .min(6)
+      .max(255)
+      .required()
+  };
+  return Joi.validate(user, schema);
+}
+
+(exports.User = User), (exports.validate = validateUser);
--- startup/routes.js
+++ startup/routes.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const feeds = require("../routes/dashboard/feeds");
+const users = require("../routes/users");
+const auth = require("../routes/auth");
 
-module.exports = function (app) {
-    app.use(express.json());
-    app.use('/api/dashboard/feeds', feeds);
-    // other routes...
-}
+module.exports = function(app) {
+  app.use(express.json());
+  app.use("/api/dashboard/feeds", feeds);
+  app.use("/api/users", users);
+  app.use("/api/auth", auth);
+  // other routes...
+};
