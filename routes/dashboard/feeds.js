b0dc9677188c89a360e8b23809c86aff3f4fbc52
Mkhgkk <emmachalz@icloud.com>
2020-04-22 01:48:04 +0800

    Feeds can match with user's courses

--- routes/dashboard/feeds.js
+++ routes/dashboard/feeds.js
@@ -1,18 +1,28 @@
 const express = require("express");
-const { Feed, validate } = require("../../models/feed")
+const { User } = require("../../models/user");
+const { Course } = require("../../models/course");
+const { Feed, validate } = require("../../models/feed");
+const auth = require("../../middleware/auth");
+const _ = require("lodash");
 const router = express.Router();
 
-router.get("/", async (req, res) => {
+router.get("/", auth, async (req, res) => {
+    const user = await User.findById(req.user._id).select("-password");
+    const selectedCourses = user.courses;
+
     const feeds = await Feed.find().select("-__v");
     if (!feeds) return res.status(404).send("No feeds found for you");
-    res.send(feeds);
+
+    const courses = _.intersection(feeds.map(feed => feed.course, selectedCourses));
+
+    res.send(courses);
 })
 
 router.post("/", async (req, res) => {
     // const { error } = validate(req.body);
     // if (error) return res.status(400).send(error.details[0].message);
 
-    let feed = new Feed({ postedBy: req.body.postedBy, type: req.body.type, content: req.body.content })
+    let feed = new Feed({ postedBy: req.body.postedBy, type: req.body.type, course: req.body.course, content: req.body.content })
     feed = await feed.save()
     res.send(feed)
 })
