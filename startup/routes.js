0f7489318bd2f67ad25180ba42667d48179d9598
Mkhgkk <emmachalz@icloud.com>
2020-04-21 22:32:40 +0800

    Added courses api

--- startup/routes.js
+++ startup/routes.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const feeds = require("../routes/dashboard/feeds");
+const courses = require("../routes/course");
 const users = require("../routes/users");
 const auth = require("../routes/auth");
 
-module.exports = function(app) {
+module.exports = function (app) {
   app.use(express.json());
   app.use("/api/dashboard/feeds", feeds);
+  app.use("/api/courses", courses);
   app.use("/api/users", users);
   app.use("/api/auth", auth);
   // other routes...
