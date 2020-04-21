cfb5c08218a9989b45bb0d4b458ac671918e0bfd
hyeyeah <dgm119@hanmail.net>
2020-04-21 22:59:56 +0900

    users : edit profile, information page

--- models/user.js
+++ models/user.js
@@ -17,18 +17,46 @@ const userSchema = new mongoose.Schema({
     minlength: 6,
     maxlength: 1024
   },
-  // newPassword: {
-  //   type: String,
-  //   minlength: 6,
-  //   maxlength: 1024
+  newPassword: {
+    type: String,
+    minlength: 6,
+    maxlength: 1024
+  },
+  userName: {
+    type: String,
+    minlength: 1,
+    maxlength: 50
+  },
+  avatar: {
+    type: String,
+    minlength: 5,
+    maxlength: 1024
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false
+  },
+  university: {
+    type: String
+  },
+  major: {
+    type: String
+  },
+  courses: {
+    type: Array
+  }
+  // university: {
+  //   type: mongoose.Schema.Types.ObjectId,
+  //   ref: "University"
   // },
-  //   name: {
-  //        type: String,
-  //        minlength: 1,
-  //        maxlength: 50
-  //     },
-
-  isAdmin: Boolean
+  // major: {
+  //   type: mongoose.Schema.Types.ObjectId,
+  //   ref: "Major"
+  // },
+  // courses: {
+  //   type: [mongoose.Schema.Types.ObjectId],
+  //   ref: "Course"
+  // }
 });
 
 userSchema.methods.generateAuthToken = function() {
@@ -46,16 +74,23 @@ function validateUser(user) {
     email: Joi.string()
       .min(5)
       .max(255)
-      .required()
       .email(),
     password: Joi.string()
       .min(6)
-      .max(255)
-      .required()
-    // newPassword: Joi.string()
-    //   .min(6)
-    //   .max(255)
-    //   .required()
+      .max(255),
+    newPassword: Joi.string()
+      .min(6)
+      .max(255),
+    userName: Joi.string()
+      .min(1)
+      .max(50),
+    avatar: Joi.string()
+      .min(5)
+      .max(1024),
+    inAdmin: Joi.boolean(),
+    university: Joi.string(),
+    major: Joi.string(),
+    courses: Joi.array()
   };
   return Joi.validate(user, schema);
 }
--- routes/users.js
+++ routes/users.js
@@ -27,7 +27,7 @@ router.post("/", async (req, res) => {
   res.header("x-auth-token", token).send(_.pick(user, ["_id", "email"]));
 });
 
-router.delete("/", async (req, res) => {
+router.delete("/", auth, async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
@@ -44,28 +44,85 @@ router.delete("/", async (req, res) => {
   res.send(user);
 });
 
-// change password
-// router.put("/", async (req, res) => {
-//   const { error } = validate(req.body);
-//   if (error) return res.status(400).send(error.details[0].message);
+router.put("/", auth, async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
 
-//   let user = await User.findOne({ email: req.body.email });
-//   if (!user) return res.status(400).send("Invalid email or password.");
+  let user = await User.findOne({ email: req.body.email });
+  if (!user) return res.status(400).send("Invalid email or password.");
 
-//   const validPassword = await bcrypt.compare(req.body.password, user.password);
-//   if (!validPassword) return res.status(400).send("Invalid email or password.");
+  const validPassword = await bcrypt.compare(req.body.password, user.password);
+  if (!validPassword) return res.status(400).send("Invalid email or password.");
 
-//   user = await User.findOneAndUpdate(
-//     { email: req.body.email },
-//     { password: req.body.newPassword }
-//   );
-//   const salt = await bcrypt.genSalt(10);
-//   user.password = await bcrypt.hash(user.password, salt);
+  user = await User.findOneAndUpdate(
+    { _id: req.user._id },
+    { $set: { password: req.body.newPassword } }
+  );
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(user.password, salt);
 
-//   await user.save();
+  await user.save();
 
-//   const token = user.generateAuthToken();
-//   res.header("x-auth-token", token).send(_.pick(user, ["_id", "email"]));
-// });
+  const token = user.generateAuthToken();
+  res.header("x-auth-token", token).send(_.pick(user, ["_id", "email"]));
+});
+
+router.put("/step1", auth, async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const user = await User.findOneAndUpdate(
+    { _id: req.user._id },
+    { $set: { userName: req.body.userName, avatar: req.body.avatar } }
+  );
+
+  if (!user) return res.status(404).send("User does not exist.");
+  res.send(user);
+});
+
+router.put("/step2", auth, async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const user = await User.findOneAndUpdate(
+    { _id: req.user._id },
+    { $set: { university: req.body.university, major: req.body.major } }
+  );
+
+  if (!user) return res.status(404).send("User does not exist.");
+  res.send(user);
+});
+
+router.put("/step3", auth, async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const user = await User.findOneAndUpdate(
+    { _id: req.user._id },
+    { $set: { courses: req.body.courses } }
+  );
+
+  if (!user) return res.status(404).send("User does not exist.");
+  res.send(user);
+});
+
+router.put("/mypage", auth, async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const user = await User.findByIdAndUpdate(
+    req.user._id,
+    {
+      userName: req.body.userName,
+      university: req.body.university,
+      major: req.body.major,
+      avatar: req.body.avatar
+    },
+    { new: true }
+  );
+
+  if (!user) return res.status(404).send("User does not exist.");
+  res.send(user);
+});
 
 module.exports = router;
